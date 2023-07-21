9c5bb713fe9e38157e3bbe2e66a4e99d961ac38c
xtianmarucco <christian.marucco@gmail.com>
2023-07-21 19:39:58 -0300

    create and delete done

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import taskReducer from "./taskReducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    tasks: taskReducer,
+});
+ export default rootReducer;
